9f1ef325df5ec3c55a75f35726f41c8cc11a2c32
deokbin <deokbin@naver.com>
2025-03-29 00:44:50 +0900

    Updates

--- assets/index-BgVUNYsK.js
+++ assets/index-BgVUNYsK.js
@@ -1,3 +1,4 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/modal-DuqQ2Q20.css"])))=>i.map(i=>d[i]);
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
@@ -45,8 +46,129 @@ function Footer() {
     <p><img src="./woowacourse_logo.png" width="180" /></p>`;
   return $footer;
 }
+const scriptRel = "modulepreload";
+const assetsURL = function(dep) {
+  return "/javascript-movie-review/" + dep;
+};
+const seen = {};
+const __vitePreload = function preload(baseModule, deps, importerUrl) {
+  let promise = Promise.resolve();
+  if (deps && deps.length > 0) {
+    document.getElementsByTagName("link");
+    const cspNonceMeta = document.querySelector(
+      "meta[property=csp-nonce]"
+    );
+    const cspNonce = (cspNonceMeta == null ? void 0 : cspNonceMeta.nonce) || (cspNonceMeta == null ? void 0 : cspNonceMeta.getAttribute("nonce"));
+    promise = Promise.allSettled(
+      deps.map((dep) => {
+        dep = assetsURL(dep);
+        if (dep in seen) return;
+        seen[dep] = true;
+        const isCss = dep.endsWith(".css");
+        const cssSelector = isCss ? '[rel="stylesheet"]' : "";
+        if (document.querySelector(`link[href="${dep}"]${cssSelector}`)) {
+          return;
+        }
+        const link = document.createElement("link");
+        link.rel = isCss ? "stylesheet" : scriptRel;
+        if (!isCss) {
+          link.as = "script";
+        }
+        link.crossOrigin = "";
+        link.href = dep;
+        if (cspNonce) {
+          link.setAttribute("nonce", cspNonce);
+        }
+        document.head.appendChild(link);
+        if (isCss) {
+          return new Promise((res, rej) => {
+            link.addEventListener("load", res);
+            link.addEventListener(
+              "error",
+              () => rej(new Error(`Unable to preload CSS for ${dep}`))
+            );
+          });
+        }
+      })
+    );
+  }
+  function handlePreloadError(err) {
+    const e = new Event("vite:preloadError", {
+      cancelable: true
+    });
+    e.payload = err;
+    window.dispatchEvent(e);
+    if (!e.defaultPrevented) {
+      throw err;
+    }
+  }
+  return promise.then((res) => {
+    for (const item of res || []) {
+      if (item.status !== "rejected") continue;
+      handlePreloadError(item.reason);
+    }
+    return baseModule().catch(handlePreloadError);
+  });
+};
+__vitePreload(() => Promise.resolve({}), true ? __vite__mapDeps([0]) : void 0);
+const Modal = ({
+  title,
+  poster_path,
+  release_date,
+  genres,
+  overview,
+  vote_average
+}) => {
+  const genreNames = genres.map((genre) => genre.name).join(", ");
+  const defaultOverview = "등록된 줄거리 정보가 없습니다.🥲";
+  return `    <div class="modal-background active" id="modalBackground">
+    <div class="modal">
+      <button class="close-modal" id="closeModal">
+        <img src="./modal_button_close.png" />
+      </button>
+      <div class="modal-container">
+        <div class="modal-image">
+          <img
+            src="https://image.tmdb.org/t/p/original/${poster_path}"
+          />
+        </div>
+        <div class="modal-description">
+          <div class="movie-description">
+            <div class="movie-title">${title}</div>
+              <p class="category">
+                ${release_date.slice(0, 4)} · ${genreNames}
+              </p>
+              <div class="rate">
+                <strong>평균</strong>
+                <img src="./star_filled.png" class="star" />
+                <div class="average_rate">${vote_average.toFixed(1)}</div>
+              </div>
+          </div>
+            
+          <hr />
+          <div class="rating">
+            <div class="caption-title"><strong>내 별점</strong></div>
+            <div class="user_rate">
+              <img src="./star_empty.png" class="rate-star" />
+              <img src="./star_empty.png" class="rate-star" />
+              <img src="./star_empty.png" class="rate-star" />
+              <img src="./star_empty.png" class="rate-star" />
+              <img src="./star_empty.png" class="rate-star" />
+            </div>
+          </div>
+          <hr />
+          <div class="movie-detail">
+            <div class="caption-title"><strong>줄거리</strong></div>
+            <div class="detail">${overview ? overview : defaultOverview}</div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
 function LogoSearchBar() {
   const container = document.createElement("div");
+  container.classList.add("logosearchbar-container");
   container.innerHTML = `
     <h1 class="logo">
       <img src="./logo.png" alt="MovieList" />
@@ -60,25 +182,24 @@ function LogoSearchBar() {
   `;
   return container;
 }
-function Header({ title, poster_path, vote_average }) {
+function Header({ title, backdrop_path, poster_path, vote_average, overview }) {
   const $header = document.createElement("header");
   $header.innerHTML = `
   <div class="background-container">
-    <div class="overlay" aria-hidden="true">  
-    </div>
-    <img src="https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${poster_path}" class="banner"/>
+    <div class="overlay" aria-hidden="true"></div>
+    <img src="https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${backdrop_path}" class="banner"/>
     <div class="top-rated-container">
-    <div class="top-rated-movie">
-      <div class="rate">
-        <img src="./star_empty.png" class="star" />
-        <span class="rate-value">${vote_average.toFixed(1)}</span>
+      <div class="top-rated-movie">
+        <div class="rate">
+          <img src="./star_empty.png" class="star" />
+          <span class="rate-value">${vote_average.toFixed(1)}</span>
+        </div>
+        <div class="title">${title}</div>
+        <button class="primary detail">자세히 보기</button>
       </div>
-      <div class="title">${title}</div>
-      <button class="primary detail">자세히 보기</button>
     </div>
   </div>
-</div>
-`;
+  `;
   return $header;
 }
 function onError(status) {
@@ -147,19 +268,18 @@ class MovieService {
     );
     return searchResult;
   }
+  async getMovieDetails(movieId) {
+    const movieDetails = await APIHandler.get(
+      `/movie/${movieId}?language=ko-KR`
+    );
+    return movieDetails;
+  }
   nextPage() {
     this.currentPage = this.currentPage + 1;
   }
   getCurrentPage() {
     return this.currentPage;
   }
-}
-function Button(text, className = "button", onClick) {
-  const $button = document.createElement("button");
-  $button.classList.add(className);
-  $button.innerText = text;
-  $button.addEventListener("click", onClick);
-  return $button;
 }
 function Skeleton() {
   const $skeletonContainer = document.createElement("li");
@@ -202,11 +322,11 @@ function MovieCaption({ title, vote_average }) {
     <img src="./star_empty.png" class="star" />
     <span>${vote_average.toFixed(1)}</span>
   </p>
-  <strong>${title}</strong>
+  <div class="movie-title">${title}</div>
 `;
   return $movieCaption;
 }
-function ThumbnailImage({ title, poster_path }) {
+function ThumbnailImage({ id, title, poster_path }) {
   if (!poster_path) {
     const $titleElement = document.createElement("div");
     $titleElement.classList.add("thumbnail-title");
@@ -215,6 +335,7 @@ function ThumbnailImage({ title, poster_path }) {
   }
   const $thumbnailImage = document.createElement("img");
   $thumbnailImage.classList.add("thumbnail");
+  $thumbnailImage.dataset.id = id;
   const defaultImage = "./default_poster_image.png";
   $thumbnailImage.src = `https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
   $thumbnailImage.alt = `${title} Thumbnail 이미지`;
@@ -223,11 +344,12 @@ function ThumbnailImage({ title, poster_path }) {
   };
   return $thumbnailImage;
 }
-function MovieItem({ title, poster_path, vote_average }) {
+function MovieItem({ id, title, poster_path, vote_average }) {
   const $movieItem = document.createElement("li");
   const $movieItemContainer = document.createElement("div");
   $movieItemContainer.classList.add("item");
   const thumbnailImage = ThumbnailImage({
+    id,
     title,
     poster_path
   });
@@ -253,6 +375,7 @@ class Movie {
   }
   movieRender() {
     return MovieItem({
+      id: this.id,
       title: this.title,
       poster_path: this.poster_path,
       vote_average: this.vote_average
@@ -280,19 +403,28 @@ const MAXIMUM_PAGE = 500;
 async function ContentsContainer(results, contentTitle) {
   var _a;
   const $main = document.querySelector("main");
-  const movieService = new MovieService();
   const $section = document.querySelector("section");
-  const isSearchMode = contentTitle.includes('"');
   const $h2 = document.createElement("h2");
   $h2.innerText = contentTitle;
   $section == null ? void 0 : $section.appendChild($h2);
   const movieList = new MovieList(results);
   const $movieList = movieList.renderMovieList();
   $section == null ? void 0 : $section.appendChild($movieList);
+  const $thumbnails = document.querySelectorAll(".thumbnail");
+  $thumbnails.forEach(($thumbnail) => {
+    $thumbnail.addEventListener("click", async () => {
+      const id = $thumbnail.dataset.id;
+      if (id) {
+        const movieService = new MovieService();
+        const movieDetails = await movieService.getMovieDetails(Number(id));
+        const event = new CustomEvent("modalOpenClicked", {
+          detail: movieDetails
+        });
+        document.dispatchEvent(event);
+      }
+    });
+  });
   hideSkeleton();
-  removeButton();
-  const $button = Button("더 보기", "more", clickMoreMovies);
-  $main == null ? void 0 : $main.appendChild($button);
   if (document.querySelector(".contentContainer")) {
     (_a = document.querySelector(".contentContainer")) == null ? void 0 : _a.remove();
   }
@@ -304,33 +436,28 @@ async function ContentsContainer(results, contentTitle) {
         <div>검색 결과가 없습니다.</div>
     `;
     $main == null ? void 0 : $main.appendChild($contentContainer);
-    removeButton();
   }
-  async function clickMoreMovies(event) {
-    movieService.nextPage();
-    showSkeleton(20, "section");
-    let additionalData;
-    if (isSearchMode) {
-      const searchQuery = contentTitle.replace(/['"]/g, "").replace(" 검색 결과", "");
-      additionalData = await movieService.getSearchResult(searchQuery);
-    } else {
-      additionalData = await movieService.getPopularMovies();
-    }
-    hideSkeleton();
-    const $moreButton = event.target;
-    const movieList2 = new MovieList(additionalData.results);
-    const $movieList2 = movieList2.renderMovieList();
-    $section == null ? void 0 : $section.appendChild($movieList2);
-    if (movieService.currentPage === MAXIMUM_PAGE || movieService.currentPage === additionalData.total_pages) {
-      $moreButton.remove();
-    }
+}
+async function handleAdditionalData(movieService, contentTitle, observer) {
+  const $section = document.querySelector("section");
+  const isSearchMode = contentTitle.includes('"');
+  movieService.nextPage();
+  showSkeleton(20, "section");
+  let additionalData;
+  if (isSearchMode) {
+    const searchQuery = contentTitle.replace(/['"]/g, "").replace(" 검색 결과", "");
+    additionalData = await movieService.getSearchResult(searchQuery);
+  } else {
+    additionalData = await movieService.getPopularMovies();
   }
-  function removeButton() {
-    const existingButton = $main == null ? void 0 : $main.querySelector("button.more");
-    if (existingButton) {
-      existingButton.remove();
-    }
+  hideSkeleton();
+  if (additionalData.results.length === 0 || movieService.getCurrentPage() >= MAXIMUM_PAGE) {
+    observer.disconnect();
+    return;
   }
+  const movieList = new MovieList(additionalData.results);
+  const $movieList = movieList.renderMovieList();
+  $section == null ? void 0 : $section.appendChild($movieList);
 }
 function HeaderSkeleton() {
   const $headerSkeletonContainer = document.createElement("header");
@@ -353,10 +480,123 @@ function HeaderSkeleton() {
   `;
   return $headerSkeletonContainer;
 }
-function renderHeader({ title, poster_path, vote_average }) {
+function openModal({
+  title,
+  release_date,
+  genres,
+  poster_path,
+  vote_average,
+  overview
+}) {
+  const $modalContainer = document.createElement("div");
+  $modalContainer.classList.add("modalcontainer");
+  const modalElement = Modal({
+    title,
+    release_date,
+    genres,
+    poster_path,
+    vote_average,
+    overview
+  });
+  $modalContainer.innerHTML = modalElement;
+  const stars = $modalContainer.querySelectorAll(".rate-star");
+  let currentRating = 0;
+  stars.forEach((star, index) => {
+    star.addEventListener("click", (event) => {
+      event.stopPropagation();
+      currentRating = index + 1;
+      updateStarDisplay(stars, currentRating);
+      saveRating(title, currentRating);
+    });
+  });
+  const savedRating = getRating(title);
+  if (savedRating) {
+    currentRating = savedRating;
+    updateStarDisplay(stars, currentRating);
+  }
+  document.body.appendChild($modalContainer);
+  const closeButton = $modalContainer.querySelector("#closeModal");
+  if (closeButton) {
+    closeButton.addEventListener("click", () => {
+      $modalContainer.remove();
+    });
+  }
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && $modalContainer) {
+      $modalContainer.remove();
+    }
+  });
+  const $modalOverlay = $modalContainer.querySelector("#modalBackground");
+  if ($modalOverlay) {
+    $modalOverlay.addEventListener("click", () => {
+      $modalContainer.remove();
+    });
+  }
+  function updateStarDisplay(stars2, rating) {
+    stars2.forEach((star, index) => {
+      if (index < rating) {
+        star.src = "./star_filled.png";
+      } else {
+        star.src = "./star_empty.png";
+      }
+    });
+    const $userRate = $modalContainer.querySelector(".user_rate");
+    const existingDescription = $userRate == null ? void 0 : $userRate.querySelector("div");
+    if (existingDescription) {
+      existingDescription.remove();
+    }
+    const rateDescription = document.createElement("div");
+    rateDescription.classList.add("rate_description");
+    switch (rating) {
+      case 1:
+        rateDescription.innerHTML = "최악이에요 (2/10)";
+        break;
+      case 2:
+        rateDescription.innerHTML = "별로예요 (4/10)";
+        break;
+      case 3:
+        rateDescription.innerHTML = "보통이에요 (6/10)";
+        break;
+      case 4:
+        rateDescription.innerHTML = "재미있어요 (8/10)";
+        break;
+      case 5:
+        rateDescription.innerHTML = "명작이에요 (10/10)";
+        break;
+    }
+    $userRate == null ? void 0 : $userRate.appendChild(rateDescription);
+  }
+  function saveRating(movieTitle, rating) {
+    const movieRatings = JSON.parse(
+      localStorage.getItem("movieRatings") || "{}"
+    );
+    movieRatings[movieTitle] = rating;
+    localStorage.setItem("movieRatings", JSON.stringify(movieRatings));
+  }
+  function getRating(movieTitle) {
+    const movieRatings = JSON.parse(
+      localStorage.getItem("movieRatings") || "{}"
+    );
+    return movieRatings[movieTitle] || null;
+  }
+}
+function renderHeader({
+  id,
+  title,
+  backdrop_path,
+  poster_path,
+  overview,
+  vote_average
+}) {
   var _a, _b;
   const $container = document.querySelector("#wrap");
-  const $header = Header({ title, poster_path, vote_average });
+  const $header = Header({
+    title,
+    backdrop_path,
+    poster_path,
+    overview,
+    vote_average
+  });
   const $logoSearchBar = LogoSearchBar();
   (_a = $header.querySelector(".top-rated-container")) == null ? void 0 : _a.prepend($logoSearchBar);
   const $headerSkeleton = (_b = document.querySelector("header .skeleton-background")) == null ? void 0 : _b.closest("header");
@@ -364,15 +604,37 @@ function renderHeader({ title, poster_path, vote_average }) {
     $headerSkeleton.remove();
   }
   $container == null ? void 0 : $container.prepend($header);
+  const $openModalButton = $header.querySelector(".detail");
+  if ($openModalButton) {
+    $openModalButton.addEventListener("click", async () => {
+      const movieService = new MovieService();
+      const movieDetails = await movieService.getMovieDetails(id);
+      const event = new CustomEvent("modalOpenClicked", {
+        detail: movieDetails
+      });
+      document.dispatchEvent(event);
+    });
+  }
 }
-async function renderContent(movieService, results) {
-  ContentsContainer(results, "지금 인기 있는 영화");
+document.addEventListener("modalOpenClicked", (event) => {
+  const customEvent = event;
+  const { title, release_date, genres, poster_path, vote_average, overview } = customEvent.detail;
+  openModal({
+    title,
+    release_date,
+    genres,
+    poster_path,
+    vote_average,
+    overview
+  });
+});
+function handleSearchEvent(movieService) {
   const $input = document.querySelector(".search-input");
   const $button = document.querySelector(".search-button");
   const $section = document.querySelector("section");
-  $input == null ? void 0 : $input.addEventListener("keypress", async (event) => {
+  $input == null ? void 0 : $input.addEventListener("keydown", async (event) => {
     const keyboardEvent = event;
-    if (keyboardEvent.key === "Enter") {
+    if (keyboardEvent.key === "Enter" && !event.isComposing) {
       const inputValue = event.target.value;
       showSkeleton(20, "section");
       if (inputValue === "") {
@@ -382,7 +644,7 @@ async function renderContent(movieService, results) {
         if ($section) {
           $section.innerHTML = "";
         }
-        ContentsContainer(searchResult.results, `"${inputValue}" 검색 결과`);
+        renderContent(searchResult.results, `"${inputValue}" 검색 결과`);
       }
     }
   });
@@ -396,9 +658,29 @@ async function renderContent(movieService, results) {
       if ($section) {
         $section.innerHTML = "";
       }
-      ContentsContainer(searchResult.results, `"${inputValue}" 검색 결과`);
+      renderContent(searchResult.results, `"${inputValue}" 검색 결과`);
     }
   });
+}
+let currentObserver = null;
+async function renderContent(results, title) {
+  if (currentObserver) {
+    currentObserver.disconnect();
+  }
+  ContentsContainer(results, title);
+  const $main = document.querySelector("main");
+  const $lastItem = document.createElement("div");
+  $lastItem.style.height = "10px";
+  $main == null ? void 0 : $main.appendChild($lastItem);
+  const movieService = new MovieService();
+  currentObserver = new IntersectionObserver((entries) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        handleAdditionalData(movieService, title, currentObserver);
+      }
+    });
+  });
+  currentObserver.observe($lastItem);
 }
 function renderFooter() {
   const $container = document.querySelector("#wrap");
@@ -414,7 +696,8 @@ async function main() {
   const data = await movieService.getPopularMovies();
   renderHeader(data.results[0]);
   hideSkeleton();
-  renderContent(movieService, data.results);
+  renderContent(data.results, "지금 인기 있는 영화");
+  handleSearchEvent(movieService);
   renderFooter();
 }
 main();
